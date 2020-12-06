4e8a4f14250d05c538b1b8ac11afeb375e695eb8
riyasaxena02 <riyaa.saxena02@gmail.com>
2020-12-06 17:11:06 +0530

    Edited validation

--- JS/validate.js
+++ JS/validate.js
@@ -1,6 +1,6 @@
 function ValidateEmail(inputText)
 {
-var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+var mailformat = /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 if(inputText.value.match(mailformat))
 {
 alert("Your details are Valid!");
